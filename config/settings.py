6f6b538f83421b7d0502aa7b06a0d7b4adc749c9
vp1961 <vp1961@list.ru>
2019-12-05 17:46:04 +0700

    Change for Heroku

--- config/settings.py
+++ config/settings.py
@@ -158,3 +158,6 @@ LEAFLET_CONFIG = {
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+
+GDAL_LIBRARY_PATH = os.environ.get('GDAL_LIBRARY_PATH')
+GEOS_LIBRARY_PATH = os.environ.get('GEOS_LIBRARY_PATH')
